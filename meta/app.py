30548b1239a19dbdf832af5f885b1e1af9b8d9da
saanviwad09 <saanviwadhwa09@gmail.com>
2026-04-06 18:39:02 +0530

    Update app.py

--- meta/app.py
+++ meta/app.py
@@ -4,18 +4,8 @@ from env.environment import CustomerSupportEnv
 app = FastAPI()
 env = CustomerSupportEnv()
 
+@app.get("/reset")
 @app.post("/reset")
 def reset():
     obs = env.reset()
     return obs.dict()
-
-@app.get("/")
-def home():
-    return {"status": "running"}
-
-def main():
-    return app
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
